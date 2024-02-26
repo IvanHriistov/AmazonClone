cdff06ca56f1d07ae8a898dc7d30b883e104bc1a
Vankata <fcbhristov@gmail.com>
2024-02-26 18:54:48 +0200

    14h - when clicking update display input and save

--- scripts/chekout.js
+++ scripts/chekout.js
@@ -131,7 +131,9 @@ document.querySelector('.js-order-summary')
       .forEach((link) => {
         link.addEventListener('click', () => {
           const productId = link.dataset.productId;
-          console.log(productId);
+
+          const container = document.querySelector(`.js-cart-item-container-${productId}`);
+          container.classList.add('is-editing-quantity');       
         });
       });
   
